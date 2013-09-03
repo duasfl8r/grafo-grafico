a7c483335571361e04f1392dd651e6319164790f
lucastx <lucas@lucasteixeira.com>
2013-09-02 23:22:14 -0300

    descomenta grupos

--- settings.py
+++ settings.py
@@ -18,40 +18,60 @@ SETTINGS = {
             'basecolor': (90, 130, 200),
             'brightness_offset': lambda: round(random.gauss(0, 40)),
         },
-#        {
-#            'basecolor': (50, 40, 156),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
-#        {
-#            'basecolor': (70, 40, 156),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
-#        {
-#            'basecolor': (180, 55, 120),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
-#        {
-#            'basecolor': (120, 60, 200),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
-#        {
-#            'basecolor': (50, 40, 130),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
-#        {
-#            'basecolor': (70, 80, 156),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
-#        {
-#            'basecolor': (100, 55, 120),
-#            'brightness_offset': lambda: round(random.gauss(0, 40)),
-#        },
+        {
+            'basecolor': (120, 130, 190),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (100, 100, 200),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (130, 70, 200),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (90, 70, 190),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (142, 76, 211),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (60, 60, 156),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (156, 60, 115),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (90, 130, 200),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (120, 130, 190),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (100, 100, 200),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (130, 70, 200),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
+        {
+            'basecolor': (90, 70, 190),
+            'brightness_offset': lambda: round(random.gauss(0, 40)),
+        },
     ],
     'group': {
-        'number_of_nodes': 40,
-        'nodes_with_extralinks': 10,
+        'number_of_nodes': 30,
+        'nodes_with_extralinks': 4,
         'intralinks_per_node': lambda: max(1, round(random.gauss(15, 3))),
-        'extralinks_per_node': lambda: max(1, round(random.gauss(2, 5))),
+        'extralinks_per_node': lambda: max(1, round(random.gauss(2, 2))),
     },
     'graphviz': {
         'graph': {
