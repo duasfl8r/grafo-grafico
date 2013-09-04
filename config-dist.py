4270779650c24c1faa0376b3429fa74a70e08bb7
lucastx <lucas@lucasteixeira.com>
2013-09-03 23:24:42 -0300

    diminui config de exemplo pra 4 grupos

--- config-dist.py
+++ config-dist.py
@@ -20,54 +20,6 @@ CONFIG = {
             'basecolor': (90, 130, 200),
             'brightness_offset': lambda: round(random.gauss(0, 40)),
         },
-        {
-            'basecolor': (120, 130, 190),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (100, 100, 200),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (130, 70, 200),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (90, 70, 190),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (142, 76, 211),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (60, 60, 156),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (156, 60, 115),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (90, 130, 200),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (120, 130, 190),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (100, 100, 200),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (130, 70, 200),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
-        {
-            'basecolor': (90, 70, 190),
-            'brightness_offset': lambda: round(random.gauss(0, 40)),
-        },
     ],
     'group': {
         'number_of_nodes': 30,
