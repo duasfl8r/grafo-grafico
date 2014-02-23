001ce25bfe94e5c84c43a3c73b799e922a07cfc2
lucastx <lucas@lucasteixeira.com>
2014-02-22 22:29:18 -0300

    altera o tamanho dos nós de acordo com as configurações

--- graph_generator.py
+++ graph_generator.py
@@ -146,6 +146,14 @@ def make_node(name, group_index, config):
 
     paint_node(node, changed_basecolor_hsv)
 
+    base_diameter = cfg('base_diameter', group_options)
+    diameter_offset = cfg('diameter_offset', group_options)
+
+    actual_diameter = base_diameter + diameter_offset
+
+    node.options['width'] = actual_diameter
+    node.options['height'] = actual_diameter
+
     return node
 
 def paint_node(node, basecolor):
