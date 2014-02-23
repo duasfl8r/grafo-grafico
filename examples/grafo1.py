74c5aeec618f1da8efef622182b5db2f12872fa5
lucastx <lucas@lucasteixeira.com>
2014-02-22 23:07:44 -0300

    bastante documentação sobre as configurações

--- examples/grafo1.py
+++ examples/grafo1.py
@@ -1,35 +1,16 @@
 # -*- encoding: utf-8 -*-
 
-"""
-Configurações do grafo-grafico.
-
-Dentro do grafo-grafico, uma configuração acessada com:
-
-    cfg('groups.0.base_color')
-
-Equivale a acessar o valor de `CONFIG` dessa forma:
-
-    CONFIG['groups'][0]['base_color']
-
-Se o valor retornado por esse acesso for uma função, ele é então
-invocado como tal e o valor retornado se torna o valor da configuração
-naquele dado acesso.
-
-Num próximo acesso à mesma configuração, a função é novamente invocada e
-um novo valor retornado.
-
-Isso permite que a configuração retorne valores aleatórios, ou usando uma
-distribuição estatística.
-"""
-
 import random
 
+# Documentação extensa sobre uso de funções e especificação de cores lá
+# embaixo.
 CONFIG = {
     # Lista de grupos e suas opções específicas
     'groups': [
         {
             # Cor base dos nós do grupo
             'basecolor': '#8e4cd3',
+
             # Variação do brilho pra cada nó específico -- altera o
             # 'value' da imagem convertida em HSV
             #
@@ -39,7 +20,8 @@ CONFIG = {
 
             # Diâmetro base dos nós do grupo, em polegadas (inches)
             'base_diameter': 1,
-            # Variação do diâmetro pra cada nó específico
+
+            # Variação do diâmetro pra cada nó específico, em polegadas
             'diameter_offset': lambda: random.gauss(0, 0.2),
         },
         {
@@ -61,28 +43,86 @@ CONFIG = {
             'diameter_offset': lambda: random.gauss(0, 0.2),
         },
     ],
+
     # Opções pra todos os grupos
     'group': {
         # Número de nós pra cada grupo
         'number_of_nodes': 30,
+
         # Número de nós com links intergrupos
         'nodes_with_extralinks': 4,
+
         # Número de intralinks -- links pra nós do mesmo grupo -- de um # nó
         'intralinks_per_node': lambda: max(1, round(random.gauss(15, 3))),
+
         # Número de interlinks -- links pra nós de outro grupo -- de um # nó
         'extralinks_per_node': lambda: max(1, round(random.gauss(2, 2))),
     },
+
     # Opções repassadas pro arquivo GV do graphviz
     'graphviz': {
         'graph': {
+            # Não permite que os nós fiquem um por cima dos outros --
+            # torna o resultado final mais bonito,
+            # na *minha* opinião ;-)
             'overlap': 'false',
+
+            # Impede que as arestas fiquem por cima dos nós
             'outputorder': 'edgesfirst',
-            'bgcolor': '#3C5F9C',
+
+            # Cor de fundo, ou 'transparent'
+            'bgcolor': 'transparent',
         },
         'node': {
+            # Remover essa opção faz os nós terem só o contorno e as
+            # arestas visíveis -- sem preenchimento.
             'style': 'filled',
-            'regular': 'true',
+
+            # Faz as configurações de diâmetro de nó funcionarem -- não
+            # calcula um tamanho 'ótimo' pros nós a partir do tamanho da
+            # tela.
             'fixedsize': 'true',
         },
     },
 }
+
+"""
+Configurações do grafo-grafico.
+
+Como as configurações são acessadas
+-----------------------------------
+
+Dentro do grafo-grafico, uma configuração acessada com:
+
+    cfg('groups.0.base_color')
+
+Equivale a acessar o valor de `CONFIG` dessa forma:
+
+    CONFIG['groups'][0]['base_color']
+
+Se o valor retornado por esse acesso for uma função, ele é então
+invocado como tal e o valor retornado se torna o valor da configuração
+naquele dado acesso.
+
+Num próximo acesso à mesma configuração, a função é novamente invocada e
+um novo valor retornado.
+
+Isso permite que a configuração retorne valores aleatórios, ou usando uma
+distribuição estatística.
+
+Cores
+-----
+
+Versão resumida: As cores devem ser especificadas em RGB, no formato "#FFFFFF", com todos
+os seis algarismos.
+
+Detalhes sórdidos:
+
+Algumas configurações de cores são manipuladas internamente pelo
+grafo-grafico -- essas devem ser *mesmo* especificadas no formato acima.
+
+Outras são repassadas diretamente pro graphviz, sendo então possível
+definir as cores em qualquer um dos formatos especificados aqui:
+
+http://www.graphviz.org/doc/info/attrs.html#k:color
+"""
