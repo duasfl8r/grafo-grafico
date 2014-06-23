947b1fd2763fa02fe8dd1ea52943db8450054228
Lucas Teixeira <lucas@lucasteixeira.com>
2014-06-22 22:39:29 -0300

    various

--- graph_generator.py
+++ graph_generator.py
@@ -5,8 +5,8 @@ import os
 import random
 from subprocess import Popen, PIPE
 
-from settings import LOGFILE
-from colors import rgb_to_hsv, hsv_to_rgb, hsv_change_brightness, rgb_hex_to_decimal, rgb_decimal_to_hex, rgb_average
+from settings import LOGFILE, DEFAULT_EDGE_COLOR, BORDER_VALUE_OFFSET
+from colors import rgb_to_hsv, hsv_to_rgb, hsv_change_brightness, rgb_average
 
 def debug(msg):
     """
@@ -25,22 +25,22 @@ class Graph:
     Create an instance and populate its nodes:
 
         >>> n1, n2, n3 = Node('n1'), Node('n2'), Node('n3')
-        >>> n1.link(n2)
-        >>> n1.link(n3)
-        >>> n2.link(n3)
+        >>> e1, e2, e3 = Edge(n1, n2), Edge(n1, n3), Edge(n2, n3)
         >>> g = Graph()
         >>> g.nodes = set([n1, n2, n3])
+        >>> g.edges = set([e1, e2, e3])
+
 
     Then, you can output a Graphviz representation:
 
         >>> g.graphviz()
         graph G {
         n1 [fillcolor="#ffffff"]
-        n1 -- n2 [color = "#000000"]
-        n1 -- n3 [color = "#000000"]
         n2 [fillcolor="#ffffff"]
-        n2 -- n3 [color = "#000000"]
         n3 [fillcolor="#ffffff"]
+        n1 -- n3 []
+        n1 -- n2 []
+        n2 -- n3 []
         }
 
     And save a PNG file rendered by Graphviz `fdp`:
@@ -62,6 +62,8 @@ class Graph:
             - filename: a relative or absolute filename
         """
         debug("Salvando como PNG em '{0}'...".format(filename))
+        if os.path.isfile(filename):
+            os.unlink(filename)
         command = ["fdp", "-T", "png", "-o", filename]
 
         process = Popen(command, stdin=PIPE)
@@ -69,7 +71,10 @@ class Graph:
 
 
     def graphviz(self):
-        content = '\n'.join(node.graphviz() for node in self.nodes) + '\n' \
+        """
+        Returns a string representing the graph, formatted as a Graphviz file.
+        """
+        content = '\n'.join(node.graphviz() for node in self.nodes) + '\n' + \
                   '\n'.join(edge.graphviz() for edge in self.edges)
 
         graph_option_tuples = [(k, v) for k, v in self.graph_options.items()]
@@ -78,9 +83,9 @@ class Graph:
         node_option_tuples = [(k, v) for k, v in self.node_options.items()]
         node_option_strs = ['node[{0}="{1}"]'.format(k, v) for k, v in node_option_tuples]
 
-        return 'graph G {\n' + \
+        return 'graph G {' + \
             '\n'.join(graph_option_strs) + '\n' + \
-            '\n'.join(node_option_strs) + '\n' + \
+            '\n'.join(node_option_strs) + \
             content + '\n' + \
         '}'
 
@@ -97,8 +102,11 @@ class Node:
         return hash(self.name)
 
     def graphviz(self):
+        """
+        Returns a string representing the graph, formatted as a Graphviz file.
+        """
         option_tuples = [(k, v) for k, v in self.options.items()]
-        option_str = '\n'.join(['{0}="{1}"'.format(k, v) for k, v in option_tuples])
+        option_str = ','.join(['{0}="{1}"'.format(k, v) for k, v in option_tuples])
 
         return '{0} [{1}]'.format(self.name, option_str)
 
@@ -110,25 +118,53 @@ class Edge:
         self.options = {}
 
     def graphviz(self):
+        """
+        Returns a string representing the graph, formatted as a Graphviz file.
+        """
         option_tuples = [(k, v) for k, v in self.options.items()]
-        option_str = '\n'.join(['{0}="{1}"'.format(k, v) for k, v in option_tuples])
+        option_str = ','.join(['{0}="{1}"'.format(k, v) for k, v in option_tuples])
 
         return '{0} -- {1} [{2}]'.format(self._n1.name, self._n2.name, option_str)
 
 
 def cfg(name, config):
-    def parse_config_file(fname):
-        """Parses the configuration file `fname`.
+    """
+    Accesses the `name` configuration inside `config`.
 
-        `fname` must be a YAML file.
+    The configuration data structure must be a `dict` containing any
+    number of any nested structure of `list`s, `dict`s and values.
 
-        Returns a configuration dictionary.
+    The config name to be accessed must be a path connected by dots, like:
+
+        groups.0.base_color
+
+    The function descends through `config`, trying ot use each component
+    first as a `dict` key, then as a `list` index, and if both fail,
+    returns `None`.
+
+    A proper configuration would then, for the config name above, access:
+
+        config['groups'][0]['base_color']
+
+    Which we could call the `VALUE`.
+
+    The function then tries to call `VALUE` as a function. If this works, it
+    returns the function value. If it fails, it returns `VALUE` itself.
+
+    This is made this way so returned values are not restricted to
+    deterministic values -- they may be functions returning random
+    values, or values from a statistic distribution.
+
+    Arguments:
+        - name: a string representing a dot-separated path
+        - config: a `dict` in the format explained above.
+    """
+
+    def filter_callable(option):
         """
-
-        with codecs.open(fname, encoding='utf-8') as yaml_file:
-            return yaml.load(yaml_file)
-
-    def eval_option(option):
+        Returns the return value of `option` if it's a callable,
+        and `option` itself otherwise.
+        """
         if hasattr(option, '__call__'):
             return option()
         return option
@@ -146,9 +182,16 @@ def cfg(name, config):
         else:
             return None
 
-    return eval_option(config)
+    return filter_callable(config)
 
-def try_to_choose_another(seq, not_this):
+def choose_another(seq, rejected):
+    """
+    Chooses randomically a value from seq which is not `rejected`.
+
+    Arguments:
+        - seq: a sequence
+        - rejected: a value from `seq`
+    """
     if len(seq) == 0:
         return None
     elif len(seq) == 1:
@@ -156,48 +199,117 @@ def try_to_choose_another(seq, not_this):
     else:
         while True:
             trial = random.choice(seq)
-            if trial != not_this:
+            if trial != rejected:
                 return trial
 
 def edge_color(n1, n2, config):
-    color = cfg('edge.color', config) or '#000000'
+    """
+    Returns the color of the edge between two nodes.
+
+    The color is defined in the 'edge.color' configuration, if it
+    exists, and on `settings.DEFAULT_EDGE_COLOR` otherwise.
+
+    If 'edge.color' is the special string 'average', the returned color
+    will be the average 'fillcolor' of the two nodes.
+
+    Otherwise, it returns the RGB color defined.
+
+    Arguments:
+        - n1: a `Node` instance
+        - n2: a `Node` instance
+        - config: the configuration `dict` (see `cfg`)
+    """
+    color = cfg('edge.color', config) or DEFAULT_EDGE_COLOR
     if color == 'average':
         color = rgb_average(n1.options['fillcolor'], n2.options['fillcolor'])
     return color
 
-def make_intragroup_links(nodes, graph, config, group_config):
+def make_intragroup_links(group, graph, config):
+    """
+    Makes random links between nodes of a same group.
+
+    Arguments:
+        - group: a `dict` containing two keys:
+            - 'nodes': a list of Node instances pertaining to this group
+            - 'config': a configuration `dict` relative to this group (a
+              part of the `config` argument)
+        - graph: a `Graph` instance representing the whole graph
+        - config: the configuration `dict` (see `cfg`)
+    """
+    nodes, group_config = group['nodes'], group['config']
     for node in nodes:
         number_of_links = int(cfg('intralinks_per_node', group_config))
         for i in range(number_of_links):
-            linked_node = try_to_choose_another(nodes, node)
+            linked_node = choose_another(nodes, node)
             edge = Edge(node, linked_node)
             edge.options['color'] = edge_color(node, linked_node, config)
             graph.edges.add(edge)
 
 def make_intergroup_links(groups, graph, config):
+    """
+    Makes random links between nodes of different groups.
+
+    Arguments:
+        - groups: a `list` of `dict`s, each containing two keys:
+            - 'nodes': a list of Node instances pertaining to this group
+            - 'config': a configuration `dict` relative to this group (a
+              part of the `config` argument)
+        - graph: a `Graph` instance representing the whole graph
+        - config: the configuration `dict` (see `cfg`)
+    """
     for group in groups:
         for i in range(cfg('nodes_with_extralinks', group['config'])):
             node = random.choice(group['nodes'])
             number_of_links = int(cfg('extralinks_per_node', group['config']))
             for i in range(number_of_links):
-                other_group = try_to_choose_another(groups, group)
+                other_group = choose_another(groups, group)
                 linked_node = random.choice(other_group['nodes'])
                 edge = Edge(node, linked_node)
                 edge.options['color'] = edge_color(node, linked_node, config)
                 graph.edges.add(edge)
 
-def make_node(name, group_index, config):
-    group_options = cfg('groups.{}'.format(group_index), config)
+def make_node(name, group_options, config):
+    """
+    Builds a `Node` instance based on group and general configurations.
+
+    Arguments:
+        - name: a string containing the name of the node
+        - group_options: a configuration `dict` relative to this group
+          (a part of the `config` argument)
+        - config: the configuration `dict` (see `cfg`)
+    """
+
+    def paint_node(node, basecolor_hsv):
+        """
+        Paints the node's fill and border colors.
+
+        The base color passed is used directly in the fill color, and
+        darkened to the border color.
+
+        Arguments:
+
+        - node: a `Node` instance; the node to be painted
+        - basecolor_hsv: a HSV color (see module documentation)
+        """
+
+        fillcolor_hsv = basecolor_hsv[::]
+        bordercolor_hsv = hsv_change_brightness(fillcolor_hsv, BORDER_VALUE_OFFSET)
+
+        fillcolor_rgb = hsv_to_rgb(fillcolor_hsv)
+        bordercolor_rgb = hsv_to_rgb(bordercolor_hsv)
+
+        node.options['color'] = '{0}'.format(bordercolor_rgb)
+        node.options['fillcolor'] = '{0}'.format(fillcolor_rgb)
 
     node = Node(name)
     node.options['label'] = ''
 
     basecolor_rgb = cfg('basecolor', group_options)
-    brightness_offset = cfg('brightness_offset', group_options)
+    value_offset = cfg('brightness_offset', group_options)
 
     basecolor_hsv = rgb_to_hsv(basecolor_rgb)
 
-    changed_basecolor_hsv = hsv_change_brightness(basecolor_hsv, brightness_offset)
+    changed_basecolor_hsv = hsv_change_brightness(basecolor_hsv, value_offset)
 
     paint_node(node, changed_basecolor_hsv)
 
@@ -212,23 +324,13 @@ def make_node(name, group_index, config):
 
     return node
 
-def paint_node(node, basecolor):
-    """
-    Args:
-
-    - color: tuple HSV format: `(hue, saturation, value)`
-    """
-
-    fillcolor = basecolor[::]
-    bordercolor = hsv_change_brightness(fillcolor, -0.4)
-
-    fillcolor_rgb = hsv_to_rgb(fillcolor)
-    bordercolor_rgb = hsv_to_rgb(bordercolor)
-
-    node.options['color'] = '{0}'.format(bordercolor_rgb)
-    node.options['fillcolor'] = '{0}'.format(fillcolor_rgb)
-
 def make_graph(config):
+    """
+    Builds a `Graph` instance based on a graph configuration.
+
+    Arguments:
+        - config: the configuration `dict` (see `cfg`)
+    """
     graph = Graph()
     graph.graph_options.update(cfg('graphviz.graph', config))
     graph.node_options.update(cfg('graphviz.node', config))
@@ -241,7 +343,7 @@ def make_graph(config):
         number_of_nodes = cfg('number_of_nodes', group_options)
         for n in range(number_of_nodes):
             name = 'g{0}_n{1}'.format(g, n)
-            node = make_node(name, g, config)
+            node = make_node(name, group_options, config)
 
             graph.nodes.add(node)
             nodes.append(node)
@@ -249,7 +351,7 @@ def make_graph(config):
         groups.append({'nodes': nodes, 'config': group_options}),
 
     for group in groups:
-        make_intragroup_links(group['nodes'], graph, config, group['config'])
+        make_intragroup_links(group, graph, config)
 
     make_intergroup_links(groups, graph, config)
 
--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@
 
 VERSION = "0.1dev"
 LOGFILE = 'graph_generator.log'
+DEFAULT_EDGE_COLOR = '#000000'
+BORDER_VALUE_OFFSET = -0.4
